10bba5ac8bceec4475be379555970b46dfc44709
inaimathi <leo.zovic@gmail.com>
2017-10-20 14:55:34 -0400

    Basic editor interface

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject knotation-editor "0.0.6-SNAPSHOT"
+(defproject knotation-editor "0.0.7-SNAPSHOT"
   :description "A front-end library implementing a Knotation editor"
   :url "http://github.com/knotation/knotation-editor"
   :license {:name "BSD 3-Clause License"
--- src/knotation_editor/editor.cljs
+++ src/knotation_editor/editor.cljs
@@ -1,6 +1,8 @@
 (ns knotation-editor.editor
   (:require [cljsjs.codemirror]
-            [modes.sparql]))
+            [modes.sparql]
+
+            [knotation-editor.styles :as styles]))
 
 (defn log! [& things]
   (apply js/console.log (map clj->js things)))
@@ -8,25 +10,17 @@
 (defn dom-loaded [fn]
   (.addEventListener js/document "DOMContentLoaded" fn))
 
-(defn editor! [editor-id & {:keys [theme mode focus?]
-                            :or {mode "sqlserver"
-                                 theme "github"
-                                 focus? true}}]
-  (log! "Testing testing! CM: " js/CodeMirror)
-  ;; (let [editor (.edit js/ace editor-id)]
-  ;;   (.setTheme editor theme)
-  ;;   (-> editor (.getSession) (.setMode mode))
-  ;;   (when focus? (.focus editor))
-  ;;   editor)
-  )
+(defn add-commands!
+  [ed commands]
+  (.setOption
+   ed "extraKeys"
+   (clj->js commands)))
 
-;; (defn add-command!
-;;   [ed name keys fn]
-;;   (.addCommand
-;;    (.-commands ed)
-;;    (clj->js {:name name :bindKey keys :exec fn})))
-
-;; (defn add-commands!
-;;   [ed & commands]
-;;   (doseq [[name keys f] commands]
-;;     (add-command! ed name keys f)))
+(defn editor!
+  [editor-selector & {:keys [mode theme focus?]
+                      :or {mode "sparql" theme "default" focus? true}}]
+  (styles/append-style!)
+  (let [editor (js/CodeMirror
+                (.querySelector js/document editor-selector)
+                (clj->js {:lineNumbers true :mode mode :autofocus focus?}))]
+    editor))
