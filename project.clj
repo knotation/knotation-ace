d08933396ca1c4de359252c6f130da4b607d4037
inaimathi <leo.zovic@gmail.com>
2017-11-10 12:13:47 -0500

    Bump version number

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject knotation-editor "0.0.12-SNAPSHOT"
+(defproject knotation-editor "0.0.13"
   :description "A front-end library implementing a Knotation editor"
   :url "http://github.com/knotation/knotation-editor"
   :license {:name "BSD 3-Clause License"
