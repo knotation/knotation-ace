5160eec2b60b48fbea1e2b7ab3338a5b95758a79
inaimathi <leo.zovic@gmail.com>
2017-12-04 19:00:28 -0500

    Compensating for multiline inputs
    This is a hack; the correct line numbers should be coming out of the
    API, but it's easier to fix here than further up

--- src/org/knotation/editor/line_map.cljs
+++ src/org/knotation/editor/line_map.cljs
@@ -10,17 +10,21 @@
   (:map
    (reduce
     (fn [memo elem]
+
       (let [ed (if (= ::st/graph-end (::st/event elem)) (+ 1 (:ed memo)) (:ed memo))
             in (::st/line-number (::st/input elem))
             out (::st/line-number (::st/output elem))
+            lns (count (::st/lines (::st/input elem)))
+            comp (:compensate memo)
             m (:map memo)]
         {:ed ed
+         :compensate (if (> lns 1) (+ (dec lns) comp) comp)
          :map (if (and in out)
                 (update-in
-                 (update-in m [ed (dec in)] #(conj (or % #{}) [:out (dec out)]))
-                 [:out (dec out)] #(conj (or % #{})  [ed (dec in)]))
+                 (update-in m [ed (dec in)] #(conj (or % #{}) [:out (+ comp (dec out))]))
+                 [:out (+ comp (dec out))] #(conj (or % #{})  [ed (dec in)]))
                 m)}))
-    {:ed 0 :map {}}
+    {:ed 0 :compensate 0 :map {}}
     compiled)))
 
 (defn lookup
